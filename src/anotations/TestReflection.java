b0591ac1434ade740fc437a2afb9d9911e70f5f0
Maks Burenko <Cherniymaks@yandex.ru>
2023-01-17 16:41:27 +0300

    123

--- src/anotations/TestReflection.java
+++ src/anotations/TestReflection.java
@@ -1,6 +1,8 @@
 package anotations;
-
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestReflection {
 
@@ -12,6 +14,23 @@ public class TestReflection {
         Class personClass2 = person.getClass();
         Class personClass3 = Class.forName("Person");
 
-        Method[] methods = personClass1.getMethods();
+//        Method[] methods = personClass1.getMethods();
+        //       for (Method method : methods) {
+//           System.out.println(method.getName() + ", "
+        //           + Arrays.toString(method.getParameterTypes()));
+        //       }
+
+        //       Field[] fields = personClass1.getDeclaredFields();
+        //       for (Field field : fields) {
+        //           System.out.println(field.getName() + ", "
+        //                   + field.getType());
+        //       }
+
+        Annotation[] annotations = personClass1.getAnnotations();
+        for (Annotation annotation : annotations) {
+            if (annotation instanceof Author) {
+                System.out.println("Yes");
+            }
+        }
     }
 }
